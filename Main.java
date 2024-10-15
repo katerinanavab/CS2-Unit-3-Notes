9f631d13d8bf72f7eacc436daea36782acb003f0
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-10-15 12:55:46 +0000

    added relational operator examples

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // One = sign ASSIGNS a value to a variable
+      int age = 15;
+      int year = 14;
+      // Two == signs is checking for EQUALITY
+      boolean match = (age == year); // returns a BOOLEAN
+      System.out.println("Does age match year?" + match);
+
+      year = 15; // RE-ASSIGNING
+      System.out.println(age == year); // true
+
+      // != means "is NOT equal to"
+      System.out.println(age != year); // false
+
+      // RELATIONAL OPERATORS 
+      // used in boolean expressions
+      double pi = 3.14159;
+      System.out.println(pi > 0); // checking if positive
+      System.out.println(pi < 0); // checking if negative
+      System.out.println(pi >= 3.14); // true
+      System.out.println(pi <= 10); // true
+
    }
 }
